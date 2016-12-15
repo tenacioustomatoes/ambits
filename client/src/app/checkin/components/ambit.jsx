010991fb57c8ff8bbf2232978d8cb97d33b600b7
davidank <dandahmann@gmail.com>
2016-12-14 18:46:56 -0800

    Fix client files

--- client/src/app/checkin/components/ambit.jsx
+++ client/src/app/checkin/components/ambit.jsx
@@ -7,8 +7,8 @@ import Snackbar from 'material-ui/Snackbar';
 import {Link} from 'react-router';
 
 const notCheckedStyle = {
-  color: 'white', //TODO: not working colors... 
-  rippleColor: 'green', 
+  color: 'white', //TODO: not working colors...
+  rippleColor: 'green',
   backgroundColor:'green',
 };
 
@@ -28,7 +28,7 @@ const cardStyle = {
 
 const linkStyle = {
   color:'white',
-  'text-decoration':'none'
+  'textDecoration':'none'
 };
 
 class Ambit extends React.Component {
@@ -51,9 +51,9 @@ class Ambit extends React.Component {
             label= {
               this.props.ambit.checkedIn ? "Checked In":"Check In!"
             }
-            onTouchTap={() => { 
-              this.props.handleCheckinAmbit(this.props.ambit); 
-              } 
+            onTouchTap={() => {
+              this.props.handleCheckinAmbit(this.props.ambit);
+              }
             }
             disabled = {this.props.ambit.checkedIn}
             style={this.props.ambit.checkedIn ? checkedStyle : notCheckedStyle}
@@ -62,7 +62,7 @@ class Ambit extends React.Component {
             label={<Link to='/display' style={linkStyle}>Stats</Link>}//send to the stats page of the ambit.
             style={statsStyle}
           />
-        </CardActions> 
+        </CardActions>
       </Card>
     );
   }
@@ -75,13 +75,3 @@ Ambit.propTypes = {
 };
 
 export default Ambit;
-
-
-
-
-
-
-
-
-
-
--- client/src/app/checkin/components/ambitList.jsx
+++ client/src/app/checkin/components/ambitList.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import Ambit from './ambit.jsx';
 
 const AmbitList = (props) => {
-  return (<div className='ambitList'> 
+  return (<div className='ambitList'>
   {
-    props.ambits.map(item => 
-      (<Ambit ambit={item} handleCheckinAmbit={props.handleCheckinAmbit}/>))
+    props.ambits.map((item, i) => (
+      <Ambit ambit={item} key={i} handleCheckinAmbit={props.handleCheckinAmbit}/>
+    ))
   }
   </div>);
 }
 
-AmbitList.propTypes = {
-  ambits: React.PropTypes.array.isRequired,
-  handleCheckinAmbit: React.PropTypes.func.isRequired
-};
+// AmbitList.propTypes = {
+//   ambits: React.PropTypes.array.isRequired,
+//   handleCheckinAmbit: React.PropTypes.func.isRequired
+// };
 
-export default AmbitList;  
-
-
+export default AmbitList;
