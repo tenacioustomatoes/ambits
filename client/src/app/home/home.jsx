f1c94305bcab864897a6f66c2aeafa74ae94f9ff
davidank <dandahmann@gmail.com>
2016-12-17 11:25:50 -0800

    Remove broadcast and stream raisedButtons

--- client/src/app/home/home.jsx
+++ client/src/app/home/home.jsx
@@ -27,27 +27,7 @@ const actionStyle = {
   position: 'fixed',
   paddingTop: '7px',
   top: '95%',
-  left: '20%',
-  height:'50px',
-  width:'240px',
-  transform: 'translate(-50%, -50%)'
-};
-const broadcastStyle = {
-  color: 'white',
-  position: 'fixed',
-  paddingTop: '7px',
-  top: '95%',
   left: '50%',
-  height:'50px',
-  width:'240px',
-  transform: 'translate(-50%, -50%)'
-};
-const streamStyle = {
-  color: 'white',
-  paddingTop: '7px',
-  position: 'fixed',
-  top: '95%',
-  left: '80%',
   height:'50px',
   width:'240px',
   transform: 'translate(-50%, -50%)'
@@ -110,21 +90,6 @@ export default class HomeView extends React.Component {
         containerElement={<Link to='/schedule' style={linkStyle} />}
         ></RaisedButton>
 
-        <RaisedButton
-        buttonStyle={broadcastStyle}
-        containerElement={<Link style={linkStyle} to='/broadcast'/>}
-        fullWidth = {true}
-        primary = {true}
-        label="BROADCAST"
-        ></RaisedButton>
-
-        <RaisedButton
-        buttonStyle={streamStyle}
-        containerElement={<Link style={linkStyle} to='/live'/>}
-        fullWidth = {true}
-        primary = {true}
-        label="STREAM"
-        ></RaisedButton>
       <BottomNavigationExampleSimple />
       </div>
     );
