883244b6014214c5ca17d379173e9da9ebe99a47
Emerson Hum <emersonhum@gmail.com>
2016-12-17 15:27:39 -0800

    Place bets on ambits and update DB

--- server/ambits/ambitModel.js
+++ server/ambits/ambitModel.js
@@ -6,7 +6,12 @@ var messageSchema = new Schema({
   avatar: String,
   name: String, 
   text: String
-})
+});
+
+var betSchema = new Schema({
+  username: String,
+  betAmount: Number
+});
 
 var ambitSchema = new Schema({
   refId: {type: Number, index: true}, //a number used to keep track of the ambit
@@ -18,7 +23,9 @@ var ambitSchema = new Schema({
   weekdays: [Boolean], //0 is Sunday, 6 is Saturday
   startDate: Date,
   checkIns: [Date], // a history of successful check-ins//time (when during the day are you supposed to check in)
-  messages: [messageSchema]
+  messages: [messageSchema],
+  bettingOdds: String,
+  bets: [betSchema]
 });
 
 var Ambit = mongoose.model('Ambit', ambitSchema);
